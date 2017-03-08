9bc269a6be94680f7b5f2bc3bbf20b9b03b5738e
Matt Hicks <matt@outr.com>
2017-03-08 09:32:24 -0600

    Finished initial configuration of console client.

--- console/js/src/main/scala/com/outr/jefe/ColorScheme.scala
+++ console/js/src/main/scala/com/outr/jefe/ColorScheme.scala
@@ -13,37 +13,40 @@ case class ColorScheme(base0: Color,
                        violet: Color,
                        blue: Color,
                        cyan: Color,
-                       green: Color)
+                       green: Color,
+                       white: Color)
 
 object ColorScheme {
   object Solarized {
     val Light: ColorScheme = ColorScheme(
-      base0 = Color.fromLong(0x839496),
-      base1 = Color.fromLong(0x93a1a1),
-      base2 = Color.fromLong(0xeee8d5),
-      base3 = Color.fromLong(0xfdf6e3),
-      yellow = Color.fromLong(0xb58900),
-      orange = Color.fromLong(0xcb4b16),
-      red = Color.fromLong(0xdc322f),
-      magenta = Color.fromLong(0xd33682),
-      violet = Color.fromLong(0x6c71c4),
-      blue = Color.fromLong(0x268bd2),
-      cyan = Color.fromLong(0x2aa198),
-      green = Color.fromLong(0x859900)
+      base0 = Color.fromLong(0x839496ff),
+      base1 = Color.fromLong(0x93a1a1ff),
+      base2 = Color.fromLong(0xeee8d5ff),
+      base3 = Color.fromLong(0xfdf6e3ff),
+      yellow = Color.fromLong(0xb58900ff),
+      orange = Color.fromLong(0xcb4b16ff),
+      red = Color.fromLong(0xdc322fff),
+      magenta = Color.fromLong(0xd33682ff),
+      violet = Color.fromLong(0x6c71c4ff),
+      blue = Color.fromLong(0x268bd2ff),
+      cyan = Color.fromLong(0x2aa198ff),
+      green = Color.fromLong(0x859900ff),
+      white = Color.White
     )
     val Dark: ColorScheme = ColorScheme(
-      base0 = Color.fromLong(0x657b83),
-      base1 = Color.fromLong(0x586e75),
-      base2 = Color.fromLong(0x073642),
-      base3 = Color.fromLong(0x002b36),
-      yellow = Color.fromLong(0xb58900),
-      orange = Color.fromLong(0xcb4b16),
-      red = Color.fromLong(0xdc322f),
-      magenta = Color.fromLong(0xd33682),
-      violet = Color.fromLong(0x6c71c4),
-      blue = Color.fromLong(0x268bd2),
-      cyan = Color.fromLong(0x2aa198),
-      green = Color.fromLong(0x859900)
+      base0 = Color.fromLong(0x657b83ff),
+      base1 = Color.fromLong(0x586e75ff),
+      base2 = Color.fromLong(0x073642ff),
+      base3 = Color.fromLong(0x002b36ff),
+      yellow = Color.fromLong(0xb58900ff),
+      orange = Color.fromLong(0xcb4b16ff),
+      red = Color.fromLong(0xdc322fff),
+      magenta = Color.fromLong(0xd33682ff),
+      violet = Color.fromLong(0x6c71c4ff),
+      blue = Color.fromLong(0x268bd2ff),
+      cyan = Color.fromLong(0x2aa198ff),
+      green = Color.fromLong(0x859900ff),
+      white = Color.White
     )
   }
 }
--- console/js/src/main/scala/com/outr/jefe/JefeConsoleClient.scala
+++ console/js/src/main/scala/com/outr/jefe/JefeConsoleClient.scala
@@ -1,18 +1,20 @@
 package com.outr.jefe
 
-import io.youi.UI
+import io.youi._
 import io.youi.app.ClientApplication
+import reactify.Var
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object JefeConsoleClient extends JefeApplication with ClientApplication {
-
+  val colorScheme: Var[ColorScheme] = Var(ColorScheme.Solarized.Dark)
 
   @JSExportTopLevel("application")
   def main(): Unit = {
     scribe.info("Client loaded!")
 
-    UI.init()
-    UI.title := "JEFE Console"
+    ui.init()
+    ui.title := "JEFE Console"
+    ui.backgroundColor := colorScheme().base3
   }
 }
