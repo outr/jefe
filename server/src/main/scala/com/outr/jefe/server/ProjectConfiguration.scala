dec294c6dcb7f2a6c62b90586d5cf6b148711f26
Matt Hicks <matt@outr.com>
2017-09-15 09:37:43 -0500

    Minor updates and testing.

--- server/src/main/scala/com/outr/jefe/server/ProjectConfiguration.scala
+++ server/src/main/scala/com/outr/jefe/server/ProjectConfiguration.scala
@@ -1,5 +1,4 @@
 package com.outr.jefe.server
 
 case class ProjectConfiguration(proxies: List[ProxyConfiguration],
-                                applications: List[ApplicationConfiguration],
-                                properties: Map[String, String])
+                                applications: List[ApplicationConfiguration])
