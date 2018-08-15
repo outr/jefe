4aba4b1f4e848222d511c85ecfa4ea0cf8483133
Matt Hicks <matt@outr.com>
2018-08-15 08:43:57 -0500

    Added support to run to support running a JAR or WAR (Resolves #10)

--- boot/src/main/scala/com/outr/jefe/boot/command/RunCommand.scala
+++ boot/src/main/scala/com/outr/jefe/boot/command/RunCommand.scala
@@ -2,7 +2,7 @@ package com.outr.jefe.boot.command
 
 import java.io.File
 
-import com.outr.jefe.application.ProcessApplication
+import com.outr.jefe.application.{Application, ProcessApplication}
 import com.outr.jefe.boot.JefeBoot
 import profig.Profig
 import com.outr.jefe.resolve._
@@ -16,7 +16,44 @@ object RunCommand extends Command {
   override def description: String = "Runs an ad-hoc process"
 
   override def execute(): Unit = {
-    Profig("arg2").as[Option[String]] match {
+    var mainClass = Profig("mainClass").as[Option[String]]
+    val workingDirectory = new File(Profig("workingDirectory").as[Option[String]].getOrElse("."))
+    val jvmArgs = loadArgs(List(
+      Profig("jvmArgs").as[Option[String]].map(new File(_)),
+      Option(new File(workingDirectory, ".jvmopts")),
+      Option(new File(".jvmopts"))
+    ).flatten: _*)
+    val args = loadArgs(List(
+      Profig("args").as[Option[String]].map(new File(_)),
+      Option(new File(workingDirectory, ".args")),
+      Option(new File(".args"))
+    ).flatten: _*)
+
+    val applicationOption: Option[Application] = Profig("arg2").as[Option[String]] match {
+      case Some(jar) if jar.toLowerCase.endsWith(".jar") => {
+        val lastSlash = jar.lastIndexOf('/')
+        val id = jar.substring(lastSlash + 1, jar.lastIndexOf('.'))
+        Some(ProcessApplication.jar(
+          id = id,
+          jars = List(new File(workingDirectory, jar)),
+          mainClass = mainClass,
+          jvmArgs = jvmArgs,
+          args = args,
+          workingDirectory = workingDirectory
+        ))
+      }
+      case Some(war) if war.toLowerCase.endsWith(".war") => {
+        val lastSlash = war.lastIndexOf('/')
+        val id = war.substring(lastSlash + 1, war.lastIndexOf('.'))
+        val port = Profig("port").as[Option[Int]].getOrElse(8080)
+        Some(ProcessApplication.war(
+          id = id,
+          war = new File(workingDirectory, war),
+          port = port,
+          jvmArgs = jvmArgs,
+          workingDirectory = workingDirectory
+        ))
+      }
       case Some(applicationInfo) => {
         val artifact: VersionedArtifact = applicationInfo match {
           case MavenVersionedRegex(groupId, artifactId, version) => {
@@ -29,22 +66,11 @@ object RunCommand extends Command {
             groupId % artifactId % version
           }
         }
-        val mainClass = Profig("mainClass").as[Option[String]].orElse(
+        mainClass = mainClass.orElse(
           JefeBoot.config(s"${artifact.group}.${artifact.name}.mainClass").as[Option[String]]
         )
-        val workingDirectory = new File(Profig("workingDirectory").as[Option[String]].getOrElse("."))
-        val jvmArgs = loadArgs(List(
-          Profig("jvmArgs").as[Option[String]].map(new File(_)),
-          Option(new File(workingDirectory, ".jvmopts")),
-          Option(new File(".jvmopts"))
-        ).flatten: _*)
-        val args = loadArgs(List(
-          Profig("args").as[Option[String]].map(new File(_)),
-          Option(new File(workingDirectory, ".args")),
-          Option(new File(".args"))
-        ).flatten: _*)
         val repositories = JefeBoot.repositories
-        val application = ProcessApplication.artifact(
+        Some(ProcessApplication.artifact(
           id = artifact.name,
           artifacts = List(artifact),
           repositories = repositories,
@@ -52,14 +78,17 @@ object RunCommand extends Command {
           jvmArgs = jvmArgs,
           args = args,
           workingDirectory = workingDirectory
-        )
-        application.start()
-        application.waitForFinished()
+        ))
       }
       case None => {
         logger.info("jefe: missing argument")
         help()
+        None
       }
+    }
+    applicationOption match {
+      case Some(application) => application.start()
+      case _ => // Nothing
     }
   }
 
@@ -74,19 +103,26 @@ object RunCommand extends Command {
   }
 
   override def help(): Unit = {
-    logger.info("Usage: jefe run groupId:artifactId(:version)")
+    logger.info("JAR Usage: jefe run path/to/a.jar")
+    logger.info("WAR Usage: jefe run path/to/a.war")
+    logger.info("Dependency Usage: jefe run groupId:artifactId(:version)")
     logger.info("")
-    logger.info("You must specify a proper Maven dependency to run.")
-    logger.info("If you exclude the version, a Jefe setting of groupId.artifactId.version will be used if specified. Otherwise, the latest release will be used.")
-    logger.info("For the version you can also specify 'latest.release' for the latest release or 'latest' for latest integration.")
-    logger.info("JVM Arguments can be provided via '.jvmopts' file found in the current directory or the 'workingDirectory'.")
-    logger.info("Arguments can be provided via '.args' file found in the current directory of the 'workingDirectory'.")
-    logger.info("By default, blocks until the process is terminated")
+    logger.info("Dependency Help:")
+    logger.info("  You must specify a proper Maven dependency to run.")
+    logger.info("  If you exclude the version, a Jefe setting of groupId.artifactId.version will be used if specified. Otherwise, the latest release will be used.")
+    logger.info("  For the version you can also specify 'latest.release' for the latest release or 'latest' for latest integration.")
+    logger.info("")
+    logger.info("General Help:")
+    logger.info("  JVM Arguments can be provided via '.jvmopts' file found in the current directory or the 'workingDirectory'.")
+    logger.info("  Arguments can be provided via '.args' file found in the current directory of the 'workingDirectory'.")
+    logger.info("  By default, blocks until the process is terminated")
+    logger.info("")
     logger.info("Arguments:")
     logger.info("  --version=???: Sets the version to be used if unspecified")
     logger.info("  --mainClass=???: Sets the main class to run. If unspecified, the manifest will be used to determine the main class to run.")
     logger.info("  --workingDirectory=???: Sets the working directory for the execution environment. If unspecified, the current directory will be used.")
     logger.info("  --jvmArgs=???: Sets the file path to find JVM arguments to be supplied to the process (line separated)")
     logger.info("  --args=???: Sets the file path to find JVM arguments to be supplied to the process (line separated)")
+    logger.info("  --port=???: Sets the port for use with running a WAR (defaults to 8080 if unspecified)")
   }
 }
